6d8a027b4153a973e115e121627f35a21212e04a
LiliwoL <github@liliwol.fr>
2023-03-22 19:42:06 +0100

    autopush

---
+++ docs/js/753-legacy.8f63b41d.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkionic_larochelle_handicap"]=self["webpackChunkionic_larochelle_handicap"]||[]).push([[753],{4753:(n,e,i)=>{i.r(e),i.d(e,{mdTransitionAnimation:()=>t});var a=i(5980),o=i(5026);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const t=(n,e)=>{var i,t,r;const l="40px",s="0px",c="back"===e.direction,d=e.enteringEl,u=e.leavingEl,m=(0,o.g)(d),b=m.querySelector("ion-toolbar"),f=(0,a.c)();if(f.addElement(m).fill("both").beforeRemoveClass("ion-page-invisible"),c?f.duration((null!==(i=e.duration)&&void 0!==i?i:0)||200).easing("cubic-bezier(0.47,0,0.745,0.715)"):f.duration((null!==(t=e.duration)&&void 0!==t?t:0)||280).easing("cubic-bezier(0.36,0.66,0.04,1)").fromTo("transform",`translateY(${l})`,`translateY(${s})`).fromTo("opacity",.01,1),b){const n=(0,a.c)();n.addElement(b),f.addAnimation(n)}if(u&&c){f.duration((null!==(r=e.duration)&&void 0!==r?r:0)||200).easing("cubic-bezier(0.47,0,0.745,0.715)");const n=(0,a.c)();n.addElement((0,o.g)(u)).onFinish((e=>{1===e&&n.elements.length>0&&n.elements[0].style.setProperty("display","none")})).fromTo("transform",`translateY(${s})`,`translateY(${l})`).fromTo("opacity",1,0),f.addAnimation(n)}return f}}}]);
+//# sourceMappingURL=753-legacy.8f63b41d.js.map
