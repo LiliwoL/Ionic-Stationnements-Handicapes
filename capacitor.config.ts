77ec05a74c35b0ccf6b1a11b7700d0628757789a
LiliwoL <github@liliwol.fr>
2023-03-22 20:04:33 +0100

    autopush

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ionic-larochelle-handicap',
-  webDir: 'dist',
+  webDir: 'docs',
   bundledWebRuntime: false
 };
 
